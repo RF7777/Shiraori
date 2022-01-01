738d20bf7d0063a4f0e7a526273a53ea0b0f7e91
RF7777 <92271299+RF7777@users.noreply.github.com>
2022-01-01 23:27:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner  = ['6281351047727', '380942618992'] // letakkan nomor kalian disini
+global.owner  = ['6282165337502', '6281270154676'] // letakkan nomor kalian disini
 global.mods  = JSON.parse(fs.readFileSync('./src/moderator.json')) // orang terpercaya?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
@@ -33,7 +33,7 @@ global.APIKeys = { // APIKey nya disini
 
 // Sticker WM
 global.packname = 'SHIRAORI️BOT'
-global.author = 'ilman'
+global.author = 'RF7'
 
 global.wait = '_*tunggu..*_'
 global.eror = '*_Server Eror_*'
